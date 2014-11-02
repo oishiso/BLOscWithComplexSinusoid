5d393b6f640b013e02cd5de3be2b7b664b5452e2
So Oishi <oishiso@gmail.com>
2014-11-02 21:58:13 +0100

    changes in comment

--- BLOscWithComplexSinusoid.cpp
+++ BLOscWithComplexSinusoid.cpp
@@ -4,7 +4,25 @@
 
 /////////////////////////////////////////////////////////////////
 
-// Band Limited Oscillator made with complex sinusoid, without wave table.
+/*
+ Band Limited Oscillator based on Peter Pabon's algorithm.
+ This version is made with complex sinusoid, without wave table.
+ Created by So Oishi on 23/06/14.
+ Copyright (c) 2014 So Oishi. All rights reserved.
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /////////////////////////////////////////////////////////////////
 
